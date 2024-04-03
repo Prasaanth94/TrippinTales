060117a7a9a3d2a8e1a23caa20b3417738957057
Edmund Lim <elimcw20@gmail.com>
2024-04-04 01:36:37 +0800

    Create Update-posts-untested

--- backend/src/routers/posts.js
+++ backend/src/routers/posts.js
@@ -7,6 +7,7 @@ const {
   getUserPosts,
   addNewPost,
   deletePost,
+  updatePost,
 } = require("../controllers/posts");
 
 router.get("/posts/seed", seedPosts);
@@ -14,5 +15,6 @@ router.get("/posts", getAllPosts);
 router.get("/:id/posts", getUserPosts);
 router.put("/posts", addNewPost);
 router.delete("/:id/posts", deletePost)
+router.post("/:id/posts",updatePost)
 
 module.exports = router;
