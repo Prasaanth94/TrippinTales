7c458736d48270ff700d04af79a3736cf8eeb622
Edmund Lim <elimcw20@gmail.com>
2024-04-04 18:32:02 +0800

    Create addCommentToPost-WIP

--- backend/src/controllers/comments.js
+++ backend/src/controllers/comments.js
@@ -18,11 +18,31 @@ const getCommentByPostId = async (req, res) => {
     console.error(error.message);
     res.json({ status: "error", msg: "error getting comment from post" });
   }
-};
+}; //WIP, need to figure how to link to post.. may need to use comments schema as a subcomponent for posts schema
+
+const addCommentToPost = async (req, res) => {
+  try {
+    const postId = req.body.postId;
+    const post = await PostsModel.findById(postId);
+    const newComment = {
+      content: req.body.content,
+      postId: postId,
+    };
+    await CommentsModel.create(newComment);
+
+    post.comments.push(createdComment._id); //possibly push into an array then map on react?
+    await post.save();
+    res.json({ status: "ok", msg: "comment added", newComment });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ status: "error", msg: "failed to add comment" });
+  }
+}; //WIP
 
 module.exports = {
   getAllComments,
   getCommentByPostId,
+  addCommentToPost,
 };
 
-//getCommentByPostId,addCommentToPost,deleteComment
+//addCommentToPost,deleteComment
--- backend/src/routers/comments.js
+++ backend/src/routers/comments.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllComments,
   getCommentByPostId,
+  addCommentToPost,
 } = require("../controllers/comments");
 
 router.get("/comments", getAllComments);
 router.get("/comments/:id", getCommentByPostId);
+router.get("/comments", addCommentToPost);
 
 module.exports = router;
