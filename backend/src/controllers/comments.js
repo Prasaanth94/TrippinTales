7898198a54ecd74dfaf3a20813a8325af48717ca
Edmund Lim <elimcw20@gmail.com>
2024-04-04 11:09:30 +0800

    Update Comments-model
    corrected error + get comment by post ID

--- backend/src/controllers/comments.js
+++ backend/src/controllers/comments.js
@@ -1,17 +1,28 @@
-const PostsModel = require("../models/Posts");
+const CommentsModel = require("../models/Comments");
 
 const getAllComments = async (req, res) => {
   try {
-    const allComments = await PostsModel.find();
-    res, json(allComments);
+    const allComments = await CommentsModel.find();
+    res.json(allComments);
   } catch (error) {
     console.error(error.message);
     res.json({ status: "error", msg: "error getting all comments" });
   }
 };
 
+const getCommentByPostId = async (req, res) => {
+  try {
+    const comment = await CommentsModel.find({ post_id: req.body.post_id });
+    res.json(comment);
+  } catch (error) {
+    console.error(error.message);
+    res.json({ status: "error", msg: "error getting comment from post" });
+  }
+};
+
 module.exports = {
   getAllComments,
+  getCommentByPostId,
 };
 
 //getCommentByPostId,addCommentToPost,deleteComment
