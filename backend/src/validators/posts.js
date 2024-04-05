33043702446954b7c2dc6f6f47a879178f7de23b
Edmund Lim <elimcw20@gmail.com>
2024-04-06 02:37:37 +0800

    Update validation-posts-reformat
    Reformatted to body in posts validations

--- backend/src/validators/posts.js
+++ backend/src/validators/posts.js
@@ -9,7 +9,7 @@ const validateBodyIdInPost = [
   body("user_id", "User ID is invalid").isLength({ min: 24, max: 24 }),
 ];
 
-const validateAddNewPost = [
+const validateBodyInPost = [
   body("title", "Title is required").not().isEmpty(),
   body(
     "title",
@@ -21,43 +21,36 @@ const validateAddNewPost = [
   body(
     "content",
     "Content may only have a minimum of 1 and a maximum of 1000 characters"
-  ).isLength({
-    min: 1,
-    max: 1000,
-  }),
-  body("url", "Url is invalid").isURL(), //TBC
-  // body("slug","").matches(something?),
-  body("tags", "Tags must have a maximum of 12 characters").isLength({
-    max: 12,
-  }),
-  // body("images","").
-  // body("meta_description", "").
-];
-
-const validateUpdatePost = [
-  body(
-    "title",
-    "Title must have a minimum of 1 and a maximum of 50 characters"
-  ).isLength({
-    min: 1,
-    max: 50,
-  }),
-  body(
-    "content",
-    "Content may only have a minimum of 1 and a maximum of 1000 characters"
-  ).isLength({
-    min: 1,
-    max: 1000,
-  }),
+  )
+    .optional()
+    .isLength({
+      min: 1,
+      max: 1000,
+    }),
   body("url", "Url is invalid").isURL(),
-  body("tags", "Tags must have a maximum of 12 characters").isLength({
-    max: 12,
-  }),
+  body(
+    "slug",
+    "Slugs should be in alphanumeric characters, optionally hyphens and underscores"
+  )
+    .optional()
+    .matches(/^[a-z0-9_-]+$/), //alphanumeric + underscore + hyphens
+  body(
+    "tags",
+    "Tags should be in alphaumeric characters, and optionally hyphens only"
+  )
+    .optional()
+    .matches(/^[\w-]+(?:,[\w-]+)*$/), //alphanumeric + hyphens only
+  body("images", "Please provide image address").optional().isURL(), //if using URL
+  body(
+    "meta_description",
+    "meta_description may only have a maximum of 200 characters"
+  )
+    .optional()
+    .isLength({ max: 200 }),
 ];
 
 module.exports = {
   validateParamIdInPost,
   validateBodyIdInPost,
-  validateAddNewPost,
-  validateUpdatePost,
+  validateBodyInPost,
 };
