c4f563d64f2671f97142da0d68dfd8086e3a580e
hongpeichua <hongggpei@gmail.com>
2024-04-05 21:42:29 +0800

    Update seed post content

--- backend/src/controllers/posts.js
+++ backend/src/controllers/posts.js
@@ -7,9 +7,10 @@ const seedPosts = async (req, res) => {
     await PostsModel.create([
       {
         _id: "660796b6e056e5bbc485eef5",
-        title: "TEST1",
-        content: "TEST1",
-        user_id: "660a5929046773e5823df208",
+        title: "Best Places To Visit In Indonesia",
+        content:
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+        user_id: "660a6f8de9abe5ec89322e64",
         url: "TEST1.com.sg",
         slug: "/TEST1",
         tags: "TEST",
@@ -18,9 +19,22 @@ const seedPosts = async (req, res) => {
       },
       {
         _id: "660796b6e056e5bbc485eef6",
-        title: "TEST2",
-        content: "TEST2",
-        user_id: "660a5bd84ed8ca64ed7020a5",
+        title: "Tasting the World One Plate at a Time",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        user_id: "660a6f8de9abe5ec89322e64",
+        url: "TEST2.com.sg",
+        slug: "/TEST2",
+        tags: "TEST",
+        images: "TEST2",
+        meta_description: "TEST2",
+      },
+      {
+        _id: "660796b6e056e5bbc485eef7",
+        title: "Tales from the Road Less Traveled",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        user_id: "660a7064e9abe5ec89322e74",
         url: "TEST2.com.sg",
         slug: "/TEST2",
         tags: "TEST",
