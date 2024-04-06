65159594172b488f0f38a82d22fe2c41bb85b755
hongpeichua <hongggpei@gmail.com>
2024-04-06 21:04:43 +0800

    Create comment section

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../context/user";
 import { useParams } from "react-router-dom";
@@ -12,6 +12,8 @@ const PostPage = () => {
   const [postDetail, setPostDetail] = useState([]);
   const fetchData = useFetch();
   const { id } = useParams();
+
+  const commentRef = useRef();
 
   const getPostById = async () => {
     const res = await fetchData(
@@ -28,9 +30,34 @@ const PostPage = () => {
     }
   };
 
+  // WIP
+  const addCommentToPost = async () => {
+    console.log(userCtx.userId); //WIP
+    const res = await fetchData(
+      "/api/add-comment",
+      "PUT",
+      {
+        userId: userCtx.userId,
+        postId: "660796b6e056e5bbc485eef5",
+        content: commentRef.current.value,
+      },
+      userCtx.accessToken
+    );
+
+    if (res.ok) {
+      getPostById();
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
   useEffect(() => {
     getPostById();
   }, [id]);
+
+  console.log("postDetail:", postDetail);
+  console.log("postDetail.comments:", postDetail && postDetail.comments);
 
   return (
     <>
@@ -40,15 +67,30 @@ const PostPage = () => {
         <div className="container content">
           <h1>{postDetail.title}</h1>
           <div className="row">
-            <div className="col-md-3">By {postDetail.user_id}</div>
-            <div className="col-md-3">{postDetail.created_at}</div>
+            <div className="col-md-6">By {postDetail.user_id}</div>
+            <div className="col-md-6">{postDetail.created_at}</div>
           </div>
           <br />
           <p>INSERT POST IMAGE HERE {postDetail.images}</p>
           <p>{postDetail.content}</p>
         </div>
+        <hr />
         <div className="container comment">
-          <h1>POPULATE COMMENT HERE</h1>
+          <h2>Comments</h2>
+          {postDetail.comments && postDetail.comments.length > 0 ? (
+            <div>
+              {postDetail.comments.map((comment, index) => (
+                <div key={index}>
+                  <strong>{comment.user_id}: </strong> {comment.content}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No comments found.</div>
+          )}
+          <br />
+          <input placeholder="Leave a comment" ref={commentRef}></input>
+          <button onClick={() => alert("comment sent")}>send</button>
         </div>
       </div>
     </>
