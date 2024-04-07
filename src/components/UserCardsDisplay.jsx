6fc2a2deff5b3facfaf8d88c1173a1a0ea296e6f
Edmund Lim <elimcw20@gmail.com>
2024-04-08 01:47:48 +0800

    Update User-Search-untested
    to test tmr

--- src/components/UserCardsDisplay.jsx
+++ src/components/UserCardsDisplay.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 const UserCardsDisplay = () => {
-  return <div></div>;
+  const fetchData = useFetch();
+  const [users, setUsers] = useState([]);
+  const [searchedUsername, setSearchedUsername] = useState(""); //for search result text
+
+  const usernameRef = useRef();
+
+  const showSearchUsers = async () => {
+    try {
+      const res = await fetchData(
+        "/api/users-username",
+        "GET",
+        { username: usernameRef.current.value },
+        userCtx.Token
+      );
+      if (res.ok) {
+        setUsers(res.data);
+        setSearchedUsername(usernameRef.current.value);
+      } else {
+        alert(JSON.stringify(res.data));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <input ref={usernameRef} type="text" placeholder="Enter username" />
+      <button onClick={showSearchUsers}>Search</button>
+      <h1>Search Results for {searchedUsername}:</h1>
+      <br />
+      <br />
+      {users.map((user, index) => (
+        <div key={index} className="searched-user-card">
+          <h3>
+            {user.first_name}
+            {""}
+            {user.last_name}
+          </h3>
+          <img src={user.image} />
+          <p>{user.greeting}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UserCardsDisplay;
@@ -10,5 +55,5 @@ export default UserCardsDisplay;
 
 //cards display the following:
 // name
-// user profile image
+// user profile image -> need payload in users?
 // greeting
