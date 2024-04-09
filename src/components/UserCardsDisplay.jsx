c9fc69b3f32b31e8f70830599dd57634b6f36ba0
Edmund Lim <elimcw20@gmail.com>
2024-04-09 22:40:44 +0800

    Update Search-Result-render
    "no user found" no longer shows until search is clicked

--- src/components/UserCardsDisplay.jsx
+++ src/components/UserCardsDisplay.jsx
@@ -2,11 +2,13 @@ import React, { useRef, useState, useContext } from "react";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../context/user";
 import { useNavigate } from "react-router-dom";
+import styles from "./UserCardsDisplay.module.css";
 
 const UserCardsDisplay = () => {
   const fetchData = useFetch();
   const [usersResult, setUsersResult] = useState([]);
   const [searchedUsername, setSearchedUsername] = useState(""); //for search result text
+  const [searchClicked, setSearchClicked] = useState(false); // to track whether search button clicked
   const userCtx = useContext(UserContext);
   const usernameRef = useRef();
   const navigate = useNavigate();
@@ -21,9 +23,8 @@ const UserCardsDisplay = () => {
       );
       if (res.ok) {
         setUsersResult(res.data.users);
-        console.log(res.data.users);
-        console.log(usersResult);
         setSearchedUsername(usernameRef.current.value);
+        setSearchClicked(true); // set to true when search button is clicked
       } else {
         alert(JSON.stringify(res.data));
       }
@@ -40,21 +41,23 @@ const UserCardsDisplay = () => {
     <div className="container">
       <input ref={usernameRef} type="text" placeholder="Enter username" />
       <button onClick={showSearchUsers}>Search</button>
+
       {searchedUsername && <h1>Search Results for {searchedUsername}:</h1>}
       {!searchedUsername && <h1>Search Results</h1>}
-      {usersResult && usersResult.length === 0 ? (
+      {searchClicked && usersResult && usersResult.length === 0 && (
         <h1>No user found.</h1>
-      ) : (
+      )}
+      {usersResult &&
+        usersResult.length > 0 &&
         usersResult.map((user, index) => (
-          <div key={index} className="searched-user-card">
+          <div key={index} className={styles["searched-user-card"]}>
+            <img src={user.image} alt={`Profile of ${user.username}`} />
             <h3 onClick={() => handleProfileNavi(user._id)}>{user.username}</h3>
             <h5>
               {user.first_name} {user.last_name}
             </h5>
-            <img src={user.image} alt={`Profile of ${user.username}`} />
           </div>
-        ))
-      )}
+        ))}
     </div>
   );
 };
